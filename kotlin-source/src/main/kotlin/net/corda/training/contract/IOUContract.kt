dd88528e85e1844c288b7869a3fd5990ff6147da
Anna Shunko <anna.shunko@zensoft.io>
2019-05-02 15:35:53 +0300

    contract

--- kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
@@ -1,8 +1,6 @@
 package net.corda.training.contract
 
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.requireSingleCommand
+import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.training.state.IOUState
 
@@ -11,6 +9,7 @@ import net.corda.training.state.IOUState
  * [IOUContractTests] for instructions on how to complete the [IOUContract] class.
  */
 class IOUContract : Contract {
+
     companion object {
         @JvmStatic
         val IOU_CONTRACT_ID = "net.corda.training.contract.IOUContract"
@@ -22,9 +21,7 @@ class IOUContract : Contract {
      * function to check for a number of commands which implement this interface.
      */
     interface Commands : CommandData {
-        // Add commands here.
-        // E.g
-        // class DoSomething : TypeOnlyCommandData(), Commands
+        class Issue : Commands
     }
 
     /**
@@ -32,9 +29,31 @@ class IOUContract : Contract {
      * The constraints are self documenting so don't require any additional explanation.
      */
     override fun verify(tx: LedgerTransaction) {
-        // Add contract code here.
-        // requireThat {
-        //     ...
-        // }
+        val command = tx.commands.requireSingleCommand(Commands.Issue::class.java)
+
+        requireThat {
+            "No inputs should be consumed when issuing an IOU." using (tx.inputStates.isEmpty())
+        }
+
+        requireThat {
+            "Only one output state should be created when issuing an IOU." using (tx.outputStates.size == 1)
+        }
+
+        val output = tx.getOutput(0) as IOUState
+        requireThat {
+            "A newly issued IOU must have a positive amount." using (output.amount.quantity > 0)
+        }
+
+        requireThat {
+            "The lender and borrower cannot have the same identity." using (output.lender != output.borrower)
+        }
+
+        requireThat {
+            "Both lender and borrower together only may sign IOU issue transaction." using
+                    (command.signers.toSet() == output.participants.map { it.owningKey }.toSet())
+        }
+
+
     }
+
 }
